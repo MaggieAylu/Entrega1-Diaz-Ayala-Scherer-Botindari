89d4bd2e64834e9d0c35a3173bd9616b6f25622e
zxFelFa <zxfabyxz@hotmail.com>
2022-10-16 17:08:06 -0300

    formulario de busqueda

--- Producto/urls.py
+++ Producto/urls.py
@@ -4,6 +4,6 @@ from Producto import views
 urlpatterns = [
     path('', views.Home, name='Home' ),
     path('about/', views.AboutUs, name='about'),
-    path('producto/', views.CargarProducto, name='producto' ),
+    path('producto/', views.CargarProducto, name='cargar-producto' ),
     path('pedido/', views.VerPedido, name='pedido'),
 ]
